64b48b43645e9b0b97841010e5bb9bee1554ded9
Anderson Chagas <aachagas@yahoo.com.br>
2021-10-02 17:38:36 -0300

    prm project

---
+++ eclipse-workspace/1Program/src/br/com/dio/PrimeiroProgram.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class PrimeiroProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello, its me/" + (a + b ));
+
+	}
+
+}
